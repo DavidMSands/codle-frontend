8480281d2a91a6e5661ac2d94ad47104e0c3b57a
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-08 10:07:38 -0800

    physical keyboard works

--- codle/src/Components/Keyboard/KeyboardSuggestions.js
+++ codle/src/Components/Keyboard/KeyboardSuggestions.js
@@ -14,6 +14,12 @@ function KeyboardSuggestions( {letters} ) {
     // find the keys with the names that match the badLetters array
     // add the class name of .suggested-off to change the bg color
     // match pattern of the board (orange, green, dark)
+
+    // check against the correct word of the day
+    // hold state for guesses tried
+    // gameboard logic:
+    // when user submits: check last guess against WOTD 
+    // if last guess includes
     
   return (
     <div>SuggestOff</div>
