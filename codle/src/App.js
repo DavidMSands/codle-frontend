ca4ff1c29eea7a42282d6802618099afa93e3e45
David <Davidmaxsands@gmail.com>
2022-03-08 10:45:44 -0700

    updated score board

--- codle/src/App.js
+++ codle/src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Header from './Components/Header/Header';
 import GameBoard from './Components/GameBoard/GameBoard';
 import Keyboard from './Components/Keyboard/Keyboard';
+import Score from './Components/ScoreBoard/Score';
 import Footer from './Components/Header/Footer';
+import { useState } from 'react'
 
 function App() {
+  const [hide, setHide] = useState(true)
+
+  function hideScore () {
+    setHide(hide => !hide)
+  }
+
   return (
     <div className="App">
       <Header />
+      <button onClick={hideScore}>Hide</button>
       <GameBoard />
+      {hide ? <Score /> : null }
       <Keyboard /> 
       <Footer /> 
     </div>
--- codle/src/Components/GameBoard/GamrRowCell.js
+++ codle/src/Components/GameBoard/GamrRowCell.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function GamrRowCell() {
   return (
-    <div>GamrRowCell</div>
+    <div></div>
   )
 }
 
