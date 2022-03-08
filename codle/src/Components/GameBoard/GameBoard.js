6104f3ec08e7da8982ef6ed2c9a7cafb64447d6f
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-07 16:03:56 -0800

    feat: fetch words in gameboard

--- codle/src/Components/GameBoard/GameBoard.js
+++ codle/src/Components/GameBoard/GameBoard.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function GameBoard() {
+  useEffect(() => {
+    fetch('http://localhost:9292/words')
+    .then(r => r.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
-    <div>GameBoard</div>
+    <div>
+    </div>
   )
 }
 
