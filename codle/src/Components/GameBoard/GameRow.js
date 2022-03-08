c640fdbf539640f740ed9b8bfe570279c75cfc82
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-08 10:03:30 -0800

    refactored styled components

--- codle/src/Components/GameBoard/GameRow.js
+++ codle/src/Components/GameBoard/GameRow.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react'
-import styled from "styled-components"
 
 import Tile from './Tile'
 
-// TODO: refactor styled components into css
-const StyledRowDiv = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 5px;
-`
 const easy = [1, 2, 3, 4]
 const medium = [1, 2, 3, 4, 5]
 const hard = [1, 2, 3, 4, 5, 6]
@@ -18,9 +10,9 @@ function GameRow() {
   const [level, setLevel] = useState(medium)
 
   return (
-    <StyledRowDiv>
+    <div className='tile'>
       {level.map(i => <Tile key={i}></Tile>)}
-    </StyledRowDiv>
+    </div>
   )
 }
 
--- codle/src/Components/GameBoard/TileContainer.js
+++ codle/src/Components/GameBoard/TileContainer.js
@@ -1,25 +1,14 @@
 import React from 'react'
-import styled from "styled-components"
 
 import GameRow from './GameRow'
-
-// TODO: refactor styled components into css
-const StyledTileContDiv = styled.div`
-  display: grid;
-  grid-template-rows: repeat(6, 1fr);
-  grid-gap: 5px;
-
-  height: 420px;
-  width: 350px;
-`
 
 function TileContainer() {
   const rows = [0, 1, 2, 3, 4, 5]
 
   return (
-    <StyledTileContDiv>
+    <div className='tile-container'>
       {rows.map(i => <GameRow key={i}></GameRow>)}
-    </StyledTileContDiv>
+    </div>
   )
 }
 
